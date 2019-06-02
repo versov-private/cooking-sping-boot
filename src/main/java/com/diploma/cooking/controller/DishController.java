a664d29ee8b8edcad485f45f296c35728bea15ad
Valentin Ersov <abrwabr97@gmail.com>
2019-06-02 20:53:24 +0300

    Update Dish controller with some request methods.

--- src/main/java/com/diploma/cooking/controller/DishController.java
+++ src/main/java/com/diploma/cooking/controller/DishController.java
@@ -4,9 +4,7 @@ import com.diploma.cooking.model.Dish;
 import com.diploma.cooking.service.DishService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,5 +29,38 @@ public class DishController {
         final Optional<Dish> dish = dishService.findById(id);
         return dish.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NO_CONTENT));
     }
+
+    @PostMapping("/dishes")
+    public ResponseEntity<Dish> save(@RequestBody Dish dish) {
+        if(dish.getId().intValue() == 0 || dish.getId() == null){
+            dishService.saveOrUpdate(dish);
+            return new ResponseEntity<>(dish, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(dish, HttpStatus.BAD_REQUEST);
+    }
+
+    @PutMapping("/dishes")
+    public ResponseEntity<Dish> update(@RequestBody Dish dish) {
+        if(dish.getId() <= 0 || dish.getId() == null){
+            return new ResponseEntity<>(dish, HttpStatus.BAD_REQUEST);
+        }
+        dishService.saveOrUpdate(dish);
+        return new ResponseEntity<>(dish, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/dishes/{id}")
+    public ResponseEntity<Dish> delete(@PathVariable Long id) {
+        final Optional<Dish> dish = dishService.findById(id);
+        return dish.map(value ->
+        {
+            dishService.delete(value);
+            return new ResponseEntity<>(value, HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.BAD_REQUEST));
+    }
+
+
+
+
+
 
 }
